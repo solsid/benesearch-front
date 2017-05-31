636eef9f08af540efe6ae8efb1aae62701f7a17b
Arnaud Oisel <arnaud.oisel@gmail.com>
2017-05-31 17:12:17 +0200

    Add photo export by team feature

--- src/app/components/photo-export/photo-export.component.ts
+++ src/app/components/photo-export/photo-export.component.ts
@@ -5,9 +5,65 @@ import { saveAs } from 'file-saver';
 @Component({
     selector: 'photo-export',
     templateUrl: './photo-export.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PhotoExportComponent {
+
+    public teams: string[] = [
+        "Accessibilité",
+        "Appro Boissons - Gratuités",
+        "Artistique - Accueil Nuit",
+        "Artistique - Chauffeur",
+        "Artistique - ET2",
+        "Artistique - Hospitality",
+        "Artistique - Régie Village",
+        "Backline",
+        "Backstage Tour",
+        "Billetterie",
+        "Boutique Merch",
+        "Campagne Printemps Solidaire",
+        "Camping",
+        "Catering",
+        "Club Solidays",
+        "Consignes Publiques",
+        "Contrôle Accès",
+        "Coordo Accueil du public",
+        "Coordo Camping",
+        "Entrée Technique 1",
+        "Entrées Public",
+        "Espace Kids",
+        "Forum Café",
+        "Greencorner",
+        "Harpic Monkeys",
+        "Images & Contenus",
+        "Invitations",
+        "La Justine",
+        "Les Volants",
+        "Liste d'attente",
+        "Loges Partenaires",
+        "Logistique",
+        "Navettes Festivalier-e-s",
+        "Parking",
+        "Patchwork",
+        "Point info / Objets trouvés",
+        "Presse",
+        "Propreté",
+        "QG Volontaires",
+        "QG des partenaires",
+        "Recharge Mobile",
+        "Relais & Silent Disco",
+        "Renfort Volontariat",
+        "Réduction des risques",
+        "Régie d'exploitation (Kalou)",
+        "Sex In The City",
+        "Studio Radio",
+        "Tirelire - La Cavalerie",
+        "Vigie",
+        "Village Solidarité"];
+
+    public enabledExport: boolean = false;
+    public enableExport: boolean = false;
+
+    private file: File;
 
     constructor(private photoExportService: PhotoExportService) {
     }
@@ -19,5 +75,42 @@ export class PhotoExportComponent {
                 saveAs(blob, 'photo_export.zip');
             }
         );
+    }
+
+    public fileChange = (event) => {
+        let fileList: FileList = event.target.files;
+        if(fileList.length > 0) {
+            this.file = fileList[0];
+        }
+        this.enableExportAction();
+    }
+
+    public exportTeamPhotos = (team: string) => {
+        this.photoExportService.exportTeamPhotos(this.file, team).subscribe(
+                res => {
+                const blob = new Blob([res], { type: 'application/octet-stream' });
+                saveAs(blob, 'photos_' + team + '_export.zip');
+                this.enableExportAction();
+            }
+        );
+        this.disableExportAction();
+    }
+
+    public exportWithoutPhoto = () => {
+        this.photoExportService.exportWithoutPhoto(this.file).subscribe(
+                res => {
+                const blob = new Blob([res], { type: 'application/octet-stream' });
+                saveAs(blob, 'sans_photo_export.zip');
+            }
+            );
+    }
+
+    private enableExportAction = () => {
+        this.enableExport = true;
+        this.enabledExport = true;
+    }
+
+    private disableExportAction = () => {
+        this.enableExport = false;
     }
 }
--- src/app/services/photo-export/photo-export.service.ts
+++ src/app/services/photo-export/photo-export.service.ts
@@ -21,4 +21,46 @@ export class PhotoExportService {
     return this.http.get('https://benesearch.herokuapp.com/photo/export/all', options)
           .map(res => res['_body']);
   }
+
+  public uploadFile = (file: File) => {
+    const formData:FormData = new FormData();
+    formData.append('file', file, file.name);
+    const headers = new Headers();
+    //headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+    const options = new RequestOptions({ headers: headers });
+    return this.http.post('https://benesearch.herokuapp.com/upload', formData, options);
+    //    .map(res => res.json());
+  }
+
+  public exportTeamPhotos = (file: File, team: string) => {
+    const formData:FormData = new FormData();
+    formData.append('file', file, file.name);
+    formData.append('team', team);
+    const headers = new Headers({
+        'Accept': 'application/zip'
+      });
+    //headers.append('Content-Type', 'multipart/form-data');
+    const options = new RequestOptions({
+        headers: headers,
+        responseType: ResponseContentType.Blob
+      });
+    return this.http.post('https://benesearch.herokuapp.com/exportTeamPhoto', formData, options)
+          .map(res => res['_body']);
+  }
+
+  public exportWithoutPhoto = (file: File) => {
+        const formData:FormData = new FormData();
+    formData.append('file', file, file.name);
+    const headers = new Headers({
+        'Accept': 'application/zip'
+      });
+    //headers.append('Content-Type', 'multipart/form-data');
+    const options = new RequestOptions({
+        headers: headers,
+        responseType: ResponseContentType.Blob
+      });
+    return this.http.post('https://benesearch.herokuapp.com/exportWithoutPhoto', formData, options)
+          .map(res => res['_body']);
+  }
 }
