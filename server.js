dbb7b7f1acc9cc4c4264ebeb8a2fb49e5131ca3e
Arnaud Oisel <arnaud.oisel@gmail.com>
2017-05-31 17:24:11 +0200

    Try to fix routing problem on Heroku

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/benevoles', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/photo-export', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
